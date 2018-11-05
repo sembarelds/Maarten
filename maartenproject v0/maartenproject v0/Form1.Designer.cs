e1cb75778c3a21467c584e040fb82adc96c118bf
sembarelds <sem.barelds@home.nl>
2018-11-05 11:26:38 +0100

    Add files via upload

---
+++ maartenproject v0/maartenproject v0/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace maartenproject_v0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button1.Location = new System.Drawing.Point(624, 234);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(284, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Game";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Exit.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Exit.Location = new System.Drawing.Point(624, 331);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(284, 65);
+            this.Exit.TabIndex = 1;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = false;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1382, 703);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.button1);
+            this.MinimumSize = new System.Drawing.Size(800, 550);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Exit;
+    }
+}
+
---
+++ maartenproject v0/maartenproject v0/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace maartenproject_v0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 secondForm;
+            secondForm = new Form2();
+            secondForm.Show();
+            this.Hide();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Controls.Clear();
+            base.BackgroundImage = System.Drawing.Image.FromFile("C:\\Users\\bakst\\OneDrive - Friesland College\\School Sem Barelds\\Jaar 1\\adult-medieval-knight-costume.jpg");
+
+        }
+    }
+    public class refer1
+    {
+        public static void variables()
+        {
+            var test1 = "hallooootjes";
+        }
+    }
